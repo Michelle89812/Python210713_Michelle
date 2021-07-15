4a7eca00b449e4258e15ec58e2b8002e11d43ec2
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-07-15 18:23:16 +0800

    Python

--- day02/HelloArgType.py
+++ day02/HelloArgType.py
@@ -1,3 +1,4 @@
+import keyword
 
 h, w, score, valid, name = 170.0, 65.5, 90, True, "小明"
 print(h, type(h))
@@ -5,3 +6,14 @@ print(w, type(w))
 print(score, type(score))
 print(valid, type(valid))
 print(name, type(name))
+
+# 保留字
+print(keyword.kwlist)
+
+# 刪除變數 ?
+amount = 100
+print("amount: ", amount)
+
+del amount
+
+print("amount: ", amount)  # 錯誤訊息 : amount' is not defined
