77355f74a856a160bd0ddc254c1fb2a388f920c0
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-07-15 20:32:48 +0800

    Python

---
+++ day02/HelloIfElse.py
@@ -0,0 +1,17 @@
+# if 判斷式
+
+n = 10
+print(n % 2)
+if n % 2 == 0:
+    print("%d 是偶數" % n)
+
+# if else 判斷式
+n = 9
+if n % 2 == 0:
+    print("%d 是偶數" % n)
+else:
+    print("%d 是奇數" % n)
+
+# if else 簡單判斷式
+print(n, "偶數" if n % 2 == 0 else "奇數")
+# if elif else 判斷式
