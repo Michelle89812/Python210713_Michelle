81091712215b1898cea6b4eb4f65625593a0722e
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-07-15 21:22:53 +0800

    Python

---
+++ day02/HelloInputBMI.py
@@ -0,0 +1,18 @@
+# 撰寫一個 BMI 系統
+# 可以輸入人名, 身高cm, 體重kg
+# 系統會算出 BMI 的結果與是否是正常, 過低(<=18), 過高(>23)
+# 資料格式呈現 : ___的身高是 ___cm 體重是 __ kg BMI 計算結果為 __.__ (____)
+# 資料範例呈現 : 小明的身高是 170cm 體重是 60 kg BMI 計算結果為 20.76 (正常)
+if __name__ == '__main__':
+    print('BMI 系統')
+    n = input("名字: ")
+    h = float(input("身高cm: "))
+    w = float(input("體重kg: "))
+    bmi = w / (h/100)**2
+    result = '正常'
+    if bmi > 23:
+        result = "過高"
+    elif bmi <= 18:
+        result = "過低"
+    print("%s的身高是 %.1fcm 體重是 %.1fkg BMI 計算結果為 %.2f (%s)" %
+          (n, h, w, bmi, result))
