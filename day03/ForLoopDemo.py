c4f8061d45428a66b02b8d532de7b8d9e3229674
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-07-20 20:06:01 +0800

    Python

---
+++ day03/ForLoopDemo.py
@@ -0,0 +1,28 @@
+'''
+迴圈 for-in、while
+for 變數 in range串列:
+    程式區塊
+'''
+
+for x in range(10):
+    print(x, end=", ")
+print("\n-------------------------------")
+
+for x in range(1, 11):  # (1~10)
+    print(x, end=", ")
+print("\n--------------------------------")
+
+for x in range(1, 11, 2):  # (1~10) 間隔 2
+    print(x, end=", ")
+print("\n--------------------------------")
+
+# break 跳出迴圈
+# continue 直接跳下一個迴圈
+
+for x in range(1, 11):  # (1~10)
+    # 資料大於 5 則迴圈停止
+    if x > 5 :
+        print('離開迴圈, x=', x)
+        break;
+    print(('x=', x))
+print('---------------------------------')
