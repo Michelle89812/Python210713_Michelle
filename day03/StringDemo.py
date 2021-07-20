7cc82e4cc08d0a1f528277ee7b90b60f3cdf2400
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-07-20 18:30:34 +0800

    Python

--- day03/StringDemo.py
+++ day03/StringDemo.py
@@ -16,7 +16,7 @@ else:
     print('{} 中沒有 {}'.format(word, keyword))
 # 是否有 sun 這個字
 keyword = 'sun'
-print(word.find(keyword))  # 會得到 shell 在 word 字串中的位置
+print(word.find(keyword))  # 會得到 shell 在 word 字串中的位置, 若得到 -1 則無此字串
 if(word.find(keyword) >= 0):
     print('{} 中有 {}'.format(word, keyword))
 else:
@@ -26,5 +26,15 @@ else:
 # She sell sea shell on the sea shore 中有 shell
 array = word.split(' ')
 print(array, array[1], len(array))
+
+# 字串索引
+# She sell sea shell on the sea shore
+print(word[0])     # 取維度=0的字元
+print(word[6])     # 取維度=6的字元
+print(word[9:18])  # 維度範圍 9<=x<18
+print(word[-4])    # 取倒數第4個字
+print(word[0:3])   # 取前3個字
+print(word[:3])    # 取前3個字(0可以省略)
+
 
 
