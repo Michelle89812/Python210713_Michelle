40794c3ff0f2226859e0a13fe8af075226b81062
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-07-20 21:52:15 +0800

    Python

--- day03/WhileLoopDemo2.py
+++ day03/WhileLoopDemo2.py
@@ -1,7 +1,31 @@
 import random
 cash = 1000
-
+limit = int(cash/3)
 while cash > 0:
     price = random.randint(1, 100)
-    cash = cash - price
-    print('買到一個禮物 %d, 剩下 $ %d ' % (price, cash))
+
+    if(cash - price >= 0):
+        cash = cash - price
+        print('禮物 $ %d (買到) 剩下 $ %d ' % (price, cash))
+
+    else:
+        print('禮物 $ %d (沒買到) 剩下 $ %d ' % (price, cash))
+        # 若還剩下 1/3 繼續買
+        if cash >= limit:
+            continue
+        else:
+            break;
+
+
+
+'''
+禮物 $ 2 (買到) 剩下 $ 175 
+禮物 $ 56 (買到) 剩下 $ 119 
+禮物 $ 92 (買到) 剩下 $ 27 
+禮物 $ 98 (買到) 剩下 $ -71 
+改良
+禮物 $ 2 (買到) 剩下 $ 175 
+禮物 $ 56 (買到) 剩下 $ 119 
+禮物 $ 92 (買到) 剩下 $ 27 
+禮物 $ 98 (沒買到) 剩下 $ 1 
+'''
