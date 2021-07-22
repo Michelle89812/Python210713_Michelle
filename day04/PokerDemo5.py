170eb01ff6dd5ca28b9b81a56212b5ec39a4b878
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-07-22 22:02:08 +0800

    Python

--- day04/PokerDemo5.py
+++ day04/PokerDemo5.py
@@ -10,18 +10,24 @@ def point(p):  # 取得點數
     return int(p)
 
 
+def getScore(cards):  # 計算總分
+    score = 0
+    for p in cards:
+        score = score + point(p)
+    return score
+
+
 if __name__ == '__main__':
     pokers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'] * 4
     r.shuffle(pokers)
     # 遊戲開始先拿到兩張牌
-    # 之後透過詢問可以延續拿牌,連續計算分數
     my_cards = []  # 目前手中的牌
     my_cards.append(pokers.pop(0))
     my_cards.append(pokers.pop(0))
-    print(my_cards)
+
     # 計算每一張牌的分數
-    my_score = 0
-    for p in my_cards:
-        my_score = my_score + point(p)
+    my_score = getScore(my_cards)
     print(my_cards, my_score)
 
+    # 之後透過詢問可以延續拿牌,連續計算分數 (Homework)
+    response = input('是否延續拿牌(Y or N): ')
