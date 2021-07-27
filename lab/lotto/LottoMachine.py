3f2b64d681737807603acc6ad54345af11ecd5b1
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-07-27 20:13:21 +0800

    Python

--- lab/lotto/LottoMachine.py
+++ lab/lotto/LottoMachine.py
@@ -3,5 +3,40 @@ Lotto 機器
 可以提供重複或不重複的彩球
 例如 : 輸入1 -> 開獎今彩539: 1~39 之間取不重複的 5 個彩球
       輸入2 -> 開獎四星彩 : 4 個 0~9 可以重複的數字
-
+      輸入0 -> Exit
 '''
+def menu():
+    print('--------------')
+    print('1. 今彩539')
+    print('2. 四星彩')
+    print('0. Exit')
+    print('--------------')
+
+def userChoice():
+    choice = int(input('請選擇: '))
+    return choice
+
+def lotto539():
+    pass
+
+def lotto4star():
+    pass
+
+if __name__ == '__main__':
+    while True:
+        # Lotto 開獎 menu()
+        menu()
+        # User 選擇
+        choice = userChoice()
+        if choice == 1:
+            pass
+        elif choice == 2:
+            pass
+        elif choice == 0:
+            print('離開程式')
+            break
+        else:
+            print('輸入錯誤，請重新輸入 !')
+            continue
+
+
