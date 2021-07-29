ed1702107b5b042630538e84e495aa7b099e5272
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-07-29 18:42:58 +0800

    Python

--- day04/PokerDemo5.py
+++ day04/PokerDemo5.py
@@ -10,31 +10,31 @@ def point(p):  # 取得點數
     return int(p)
 
 
-def getScore(cards):  # 計算總分
+def getScore(cards): # 計算總分
     score = 0
     for p in cards:
         score = score + point(p)
     return score
 
 
-if __name__ == '__main__':
+def playGame():
     pokers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'] * 4 * 4
     r.shuffle(pokers)
-    # 遊戲開始先拿到兩張牌
-    my_cards = []   # 目前User手中的牌
+
+    # 之後透過詢問可以連續拿牌,連續計算分數 (Homework)
+    # 遊戲開始User與PC各先拿到一張牌
+    my_cards = []  # 目前 user 手中的牌
     my_cards.append(pokers.pop(0))
-    pc_cards = []   # 目前PC手中的牌
+    pc_cards = []  # 目前 pc 手中的牌
     pc_cards.append(pokers.pop(0))
 
-
-    # 之後透過詢問可以延續拿牌,連續計算分數 (Homework)
     # User
     while True:
         # 計算每一張牌的分數
         my_score = getScore(my_cards)
         print("User:", my_cards, my_score)
         # 分數是否超過 10.5
-        if (my_score > 10.5):
+        if(my_score > 10.5):
             print('User 爆了 !')
             break;
         resp = input('User 還要牌嗎(y/n) ? ')
@@ -50,24 +50,29 @@ if __name__ == '__main__':
         if (pc_score > 10.5):
             print('PC 爆了 !')
             break;
-        if pc_score < 7:  # 小於 7 點強制補牌
+        if pc_score < 7: # 小於 7 點強制補牌
             pc_cards.append(pokers.pop(0))
         else:
             break;
 
-    # Winner?
+    # Winner ?
     print("User:", my_score, " PC:", pc_score)
     if my_score <= 10.5 and pc_score <= 10.5:
         if my_score > pc_score:
             print("Winner: User")
+            return 1
         elif my_score < pc_score:
             print("Winner: PC")
+            return -1
         else:
             print("平手")
+            return 0
     elif my_score <= 10.5 and pc_score >= 10.5:
         print("Winner: User")
+        return 1
     elif my_score > 10.5 and pc_score <= 10.5:
         print("Winner: PC")
+        return -1
     else:
         print("平手(都爆了)")
-
+        return 0
---
+++ day04/PokerDemo6.py
@@ -0,0 +1,7 @@
+# 10點半對戰
+# 今天你有100元
+# 可以自由下注
+import day04.PokerDemo5 as poker
+
+if __name__ == '__main__':
+    poker.playGame()
