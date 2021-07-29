3f83556e86c547e5853e6937a8e8922a11662478
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-07-29 20:21:56 +0800

    Python

--- day06/Hellojson.py
+++ day06/Hellojson.py
@@ -4,3 +4,19 @@ x = '{"name":"John", "age":18}'
 person = json.loads(x)
 print(type(person))
 print(person['name'], person['age'])
+
+x = '{"name":"John", "age":18, "profile":{"w": 60.0, "h":170.0}}'
+# 計算出 John 的 bmi
+# bmi = w / (h/100)**2
+person = json.loads(x)
+profile = person["profile"]
+print(type(profile), profile)
+w = profile['w']
+h = profile['h']
+print(type(w), w, type(h), h)
+bmi = w / (h/100)**2
+print(bmi)
+
+
+
+
