718ca4511047fc81fea9699865af3ae7c9239f35
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-07-29 19:25:27 +0800

    Python

--- day05/DataStructureSplit.py
+++ day05/DataStructureSplit.py
@@ -9,3 +9,11 @@ print(3, exam_dict.values())
 print(4, list(exam_dict.values()))
 print(5, list(map(int, exam_dict.values())))
 print(6, sum(list(map(int, exam_dict.values()))))
+
+# map
+s = '1 3 5 7'
+s_list = s.split()  # 空括號 = 空白切割
+print(s, s_list)
+s_int_list = list(map(int, s_list))
+print(s_list)
+print(s_int_list)
