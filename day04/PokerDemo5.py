1fc8d9db7834ad15f1271a81ab3c74f4b74cbea2
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-07-29 18:31:12 +0800

    Python

--- day04/PokerDemo5.py
+++ day04/PokerDemo5.py
@@ -18,24 +18,56 @@ def getScore(cards):  # 計算總分
 
 
 if __name__ == '__main__':
-    pokers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'] * 4
+    pokers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'] * 4 * 4
     r.shuffle(pokers)
     # 遊戲開始先拿到兩張牌
-    my_cards = []  # 目前手中的牌
+    my_cards = []   # 目前User手中的牌
     my_cards.append(pokers.pop(0))
+    pc_cards = []   # 目前PC手中的牌
+    pc_cards.append(pokers.pop(0))
 
 
     # 之後透過詢問可以延續拿牌,連續計算分數 (Homework)
+    # User
     while True:
         # 計算每一張牌的分數
         my_score = getScore(my_cards)
-        print(my_cards, my_score)
+        print("User:", my_cards, my_score)
         # 分數是否超過 10.5
-        if my_score > 10.5:
-            print('爆了!')
-            break
-        resp = input('繼續拿牌嗎(y/n)? ')
+        if (my_score > 10.5):
+            print('User 爆了 !')
+            break;
+        resp = input('User 還要牌嗎(y/n) ? ')
         if resp == 'y':
             my_cards.append(pokers.pop(0))
         else:
             break
+    # PC
+    while True:
+        # 計算每一張牌的分數
+        pc_score = getScore(pc_cards)
+        print("PC:", pc_cards, pc_score)
+        if (pc_score > 10.5):
+            print('PC 爆了 !')
+            break;
+        if pc_score < 7:  # 小於 7 點強制補牌
+            pc_cards.append(pokers.pop(0))
+        else:
+            break;
+
+    # Winner?
+    print("User:", my_score, " PC:", pc_score)
+    if my_score <= 10.5 and pc_score <= 10.5:
+        if my_score > pc_score:
+            print("Winner: User")
+        elif my_score < pc_score:
+            print("Winner: PC")
+        else:
+            print("平手")
+    elif my_score <= 10.5 and pc_score >= 10.5:
+        print("Winner: User")
+    elif my_score > 10.5 and pc_score <= 10.5:
+        print("Winner: PC")
+    else:
+        print("平手(都爆了)")
+
