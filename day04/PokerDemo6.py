baa74e7a3445706519c0d2efe9d44428dde06ef3
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-07-29 19:03:02 +0800

    Python

--- day04/PokerDemo6.py
+++ day04/PokerDemo6.py
@@ -4,4 +4,28 @@
 import day04.PokerDemo5 as poker
 
 if __name__ == '__main__':
-    poker.playGame()
+    balance = 100
+    while True:
+        print('balance:', balance)
+        if balance == 0:
+            print('你沒錢了，請離開遊戲...')
+            break
+
+
+        bet = int(input('請下注(0:離開): '))
+
+        if bet == 0 and balance == 0:
+            break
+        elif bet > balance:
+            print('下注金額不可超過balance，請重新下注')
+            continue
+        elif bet < 0:
+            print('下注金額不可為負值')
+            continue
+
+        result = poker.playGame()
+        # print(result)
+        balance = balance + (result * bet)
+        # print(balance)
+
+
