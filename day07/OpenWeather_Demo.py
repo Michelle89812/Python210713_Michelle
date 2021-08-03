0a57b2867e028ac37af26d2777c6995807dc792a
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-08-03 21:17:08 +0800

    Python

--- day07/OpenWeather_Demo.py
+++ day07/OpenWeather_Demo.py
@@ -3,5 +3,20 @@ import requests
 key = '6f6e5160e0d2b4b019e1ccff3232b603'  # 用自己的 key
 q = 'taoyuan,tw'
 url = 'https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s' % (q, key)
-print(url)
 
+# description, temp(-273.75), feels_like, humidity
+# 1. 抓取文字資料
+data = requests.get(url).text
+# 2. 將文字資料轉成 json 物件(結構化的dict), 目的: 便於日後分析
+root = json.loads(data)
+# 3. 分析資料
+description = root['weather'][0]['description']
+temp = root['main']['temp'] - 273.15
+feels_like = root['main']['feels_like'] - 273.15
+humidity = root['main']['humidity']
+# 4. 列印資料
+print('地區: %s' % q)
+print('天氣概述: %s' % description)
+print('溫度(°C): %.2f' % temp)
+print('體感(°C): %.2f' % feels_like)
+print('濕度(%%): %d' % humidity)
