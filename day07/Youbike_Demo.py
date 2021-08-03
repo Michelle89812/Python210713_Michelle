42572de68abbf0d5ad6d0b49259f998e6c7e3dc6
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-08-03 19:01:42 +0800

    Python

--- day07/Youbike_Demo.py
+++ day07/Youbike_Demo.py
@@ -8,4 +8,9 @@ print(youbike['retVal']['2001']['sna'])
 print(youbike['retVal']['2001']['tot'])
 print(youbike['retVal']['2001']['sbi'])
 print(youbike['retVal']['2001']['bemp'])
+print(youbike['retVal']['2001']['lat'])
+print(youbike['retVal']['2001']['lng'])
+
+# 我所在地的經緯度 25.051249173402592, 121.28611345321114 蘆竹區聯福街聯福巷61-10號
+
 
