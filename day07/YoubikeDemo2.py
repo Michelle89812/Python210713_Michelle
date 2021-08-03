e67aed34349686d6c71a63343dcdaf7a922b475e
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-08-03 20:34:59 +0800

    Python

--- day07/YoubikeDemo2.py
+++ day07/YoubikeDemo2.py
@@ -1,18 +1,24 @@
 import json
-file = open('桃園公共自行車即時服務資料.json', 'r', encoding='utf-8')
-data = file.read()
+import day07.Util as u
+import requests
+
+# file = open('桃園公共自行車即時服務資料.json', 'r', encoding='utf-8')
+# data = file.read()
+url = 'https://data.tycg.gov.tw/opendata/datalist/datasetMeta/download?id=5ca2bfc7-9ace-4719-88ae-4034b9a5a55c&rid=a1b4714b-3b75-4ff8-a8f2-cc377e4eaa0f'
+data = requests.get(url).text
 youbike = json.loads(data)
+
 
 # Lab 請列出所有站台的名稱 sna
 # 提示利用 for 迴圈
 # 串列變數 = range(起始值, 終止值)  # 不含終止值
-for no in range(2001, 2412):
-    sno = str(no)
-    try:
-        sna = youbike['retVal'][sno]['sna']
-        print(sno, sna)
-    except:
-        pass
+# for no in range(2001, 2412):
+#     sno = str(no)
+#     try:
+#         sna = youbike['retVal'][sno]['sna']
+#         print(sno, sna)
+#     except:
+#         pass
 
 # 我所在地的經緯度 24.99047, 121.31187 桃園市民權路6號
 my_lat = 24.99047
