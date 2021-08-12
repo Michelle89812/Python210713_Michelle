db8422af166e2933d2659660fce5fe2a23e2129c
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-08-12 19:24:16 +0800

    Python

---
+++ day10/OO9.py
@@ -0,0 +1,22 @@
+class USD:
+    def __get__(self, instance, owner):
+        ntd = instance.ntd
+        return ntd / 30.5
+
+class JPY:
+    def __get__(self, instance, owner):
+        ntd = instance.ntd
+        return ntd / 0.28
+
+class Exchange:
+    usd = USD()
+    jpy = JPY()
+
+    def __init__(self, ntd) -> None:
+        self.ntd = ntd
+
+
+if __name__ == '__main__':
+    ex = Exchange(10000)
+    print('%.2f' % ex.usd)
+    print('%.2f' % ex.jpy)
