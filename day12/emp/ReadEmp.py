f4e0e5a75280108516f57dc4fef0a88630857f64
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-08-17 21:40:00 +0800

    Python

--- day12/emp/ReadEmp.py
+++ day12/emp/ReadEmp.py
@@ -11,12 +11,12 @@ names = [header[1] for header in cursor.fetchall()]
 for name in names:
     print(name, end='\t')
 
-print('\n_______________________________________________')
+print('\n______________________________________________________________')
 
 sql = 'SELECT emp_id, dept_id, emp_name, emp_salary, create_date FROM employees'
 cursor.execute(sql)
 emps = cursor.fetchall()
 
 for emp in emps:
-    print(('%-7d\t%-7d\t%-8s\t%10s\t%s\t' % (emp[0], emp[1], emp[2], ('$' + str(emp[3])), emp[4])))
+    print(('%6d\t%7d\t%-8s\t%10s\t%s\t' % (emp[0], emp[1], emp[2], ('$' + str(emp[3])), emp[4])))
 
