e7df5defca0af422bbe23ec820b869257a049d62
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-08-19 20:23:57 +0800

    Python

--- day12/lotto/SelectLotto.py
+++ day12/lotto/SelectLotto.py
@@ -4,15 +4,28 @@ import sqlite3
 conn = sqlite3.connect('lotto.db')
 cursor = conn.cursor()
 
+list = []
 for i in range(49):
     n = i+1
     sql = "select count(*) as count from lotto " \
           "where n1=%d or n2=%d or n3=%d or n4=%d or n5=%d or n6=%d" % (n, n, n, n, n, n)
     cursor.execute(sql)
     count = cursor.fetchone()
-    print(n, count[0])
+
+    list.append({'n': n, 'num': count[0]})
+    # print(n, count[0])
 
 conn.close()
 
 # 請問哪一個號碼出現次數最多 ?
 # 以及哪一個號碼出現次數最少 ?
+seq = [x['num'] for x in list]
+print(seq)
+max = max(seq)
+min = min(seq)
+
+for x in list:
+    if x['num'] == max:
+        print('max:', x)
+    if x['num'] == min:
+        print('min:', x)
