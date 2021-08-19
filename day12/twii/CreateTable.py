e7de91cbbdd06a8c5850899d85d84d7b057df79f
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-08-19 21:07:30 +0800

    Python

---
+++ day12/twii/CreateTable.py
@@ -0,0 +1,23 @@
+# 建立 twii.db
+import sqlite3
+
+sql = 'create table if not exists twii(' \
+      'id integer primary key autoincrement,' \
+      'stock_code varchar (10),' \
+      'securities_name varchar (20),' \
+      'yield_rate double,' \
+      'div_year integer,' \
+      'pe double,' \
+      'pb double,' \
+      'fin_report varchar (10),' \
+      'memo varchar (50)' \
+      ')'
+
+conn = sqlite3.connect('twii.db')
+cursor = conn.cursor()
+cursor.execute(sql)
+
+conn.commit()
+conn.close()
+
+print('twii 資料表建立成功')
