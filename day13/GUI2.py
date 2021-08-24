4287f430e3c34c6cd52984c2379e9a7791858cc8
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-08-24 20:09:59 +0800

    Python

--- day13/GUI2.py
+++ day13/GUI2.py
@@ -1,11 +1,19 @@
 # 放入視窗元件
 import tkinter
+from tkinter import messagebox
 '''
 +-----------+
 |   Label   |
 | B1     B2 |
 +-----------+
 '''
+
+def show_message():
+    messagebox.showinfo('Hello', 'Message...')
+
+def win_exit():
+    win.quit()
+
 
 win = tkinter.Tk()
 
@@ -20,10 +28,12 @@ label = tkinter.Label(win,
                       height=5)
 label.pack()
 
-button1 = tkinter.Button(win, text="OK", width=10, height=2, font=('Arial', 20))
+button1 = tkinter.Button(win, text="OK", width=10, height=2, font=('Arial', 20),
+                         command=show_message)
 button1.pack(side=tkinter.LEFT)
 
-button2 = tkinter.Button(win, text="Cancel", width=10, height=2, font=('Arial', 20))
+button2 = tkinter.Button(win, text="Cancel", width=10, height=2, font=('Arial', 20),
+                         command=win_exit)
 button2.pack(side=tkinter.RIGHT)
 
 win.mainloop()
