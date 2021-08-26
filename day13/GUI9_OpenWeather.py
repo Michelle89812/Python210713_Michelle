d1de87e6fa3bd968f800ae4eb1a4fe8437897977
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-08-26 22:02:31 +0800

    Python

--- day13/GUI9_OpenWeather.py
+++ day13/GUI9_OpenWeather.py
@@ -19,14 +19,25 @@ import tkinter
 import requests
 import json
 import urllib
+import threading
 from PIL import Image, ImageTk
 from io import BytesIO
 from tkinter import font
 
 def showIcon(icon):
     path = 'https://openweathermap.org/img/wn/%s@4x.png' % icon
+    # 1. 圖片原始資料
     raw_data = urllib.request.urlopen(path).read()
     print(raw_data)
+    # 2. 將圖片原始資料轉成 Image 物件
+    img = Image.open(BytesIO(raw_data))
+    # 3. 將 Image 物件轉成 Photo 物件 (給 Label 使用)
+    photo = ImageTk.PhotoImage(img)
+    # 4. 重新配置
+    icon_label = tkinter.Label(image=photo)
+    icon_label.image = photo
+    icon_label.grid(row=4, column=0, columnspan=2, sticky='EWNS')
+
 
 def openweatherService(q):
     path = 'https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s' % (q, appid)
@@ -41,12 +52,16 @@ def openweatherService(q):
     t_result_value.set('%.1f°C' % temp)
     h_result_value.set('%.1f %%' % humidity)
     desp_label_value.set(description)
-    showIcon(icon)
+    # 加入執行緒 2
+    t2 = threading.Thread(target=showIcon, args=(icon,))
+    t2.start()
 
 
 def submit():
     q = entry.get()
-    openweatherService(q)
+    # 加入執行緒 1
+    t1 = threading.Thread(target=openweatherService, args=(q,))
+    t1.start()
 
 
 if __name__ == '__main__':
@@ -54,27 +69,27 @@ if __name__ == '__main__':
 
     win = tkinter.Tk()
     win.title('Open Weather')
-    win.geometry('500x200')
+    win.geometry('500x500')
 
     myfont = font.Font(family='Arial', size=24, weight='bold')
 
     entry = tkinter.Entry(font=myfont, justify=tkinter.CENTER)
     entry.insert(0, 'taoyuan,tw')
-    submit_btn = tkinter.Button(text='Submit', font=myfont, command=submit)
+    submit_btn = tkinter.Button(text='查詢', font=myfont, command=submit)
 
     t_result_value = tkinter.StringVar()
     t_result_value.set('0.0°C')
-    t_label = tkinter.Label(text='temperature', font=myfont)
+    t_label = tkinter.Label(text='溫度', font=myfont)
     t_result = tkinter.Label(textvariable=t_result_value, font=myfont)
 
     h_result_value = tkinter.StringVar()
     h_result_value.set('0.0%')
-    h_label = tkinter.Label(text='humidity', font=myfont)
+    h_label = tkinter.Label(text='濕度', font=myfont)
     h_result = tkinter.Label(textvariable=h_result_value, font=myfont)
 
     desp_label_value = tkinter.StringVar()
     desp_label = tkinter.Label(textvariable=desp_label_value, font=myfont)
-    icon_label = tkinter.Label(text='?', font=myfont)
+    icon_label = tkinter.Label(text='photo', font=myfont)
 
 
     win.rowconfigure((0, 1), weight=1)  # 列 0, 1 同步放大與縮小
