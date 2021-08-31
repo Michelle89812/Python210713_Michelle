8521a7f54833b517e7faf4bd89006d6ce05588a1
黃思瑀 Michelle <87371192+Michelle89812@users.noreply.github.com>
2021-08-31 19:23:51 +0800

    Python

---
+++ day14/OpenCV_Image_FaceTesting.py
@@ -0,0 +1,32 @@
+import cv2
+
+# 人臉特徵檔
+face_Cascade = cv2.CascadeClassifier('./xml/haarcascade_frontalface_alt.xml')
+
+# 讀檔
+frame = cv2.imread('./image/test.jpg')
+
+# 將彩色圖片(RGB)進行灰階(Gray)處理
+gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+# face 的座標範圍
+faces = face_Cascade.detectMultiScale(
+    gray,  # 待檢測圖片
+    scaleFactor=1.1,  # 檢測粒度(越小越精準(速度慢)；越大越模糊(速度快))
+    minNeighbors=5,  # 每個目標至少檢測到幾次成功，才被認定是 face
+    minSize=(30, 30),  # 設定數據最小搜尋的尺寸
+    flags=cv2.CASCADE_SCALE_IMAGE
+)
+print(faces)
+
+# 在 face 周圍畫上矩形框
+for (x, y, w, h) in faces:                  # B  G  R,  2=線框寬度
+    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+# 將 frame 顯示出來
+cv2.imshow('my window', frame)
+
+# 按下任一鍵離開程式
+c = cv2.waitKey(0)
+print(c)
+
+cv2.destroyWindow('my window')
